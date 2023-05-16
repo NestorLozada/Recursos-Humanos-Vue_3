1d9c6bba73823f5b695d45ed97195327488e2813
NestoLozada <martinlozada1@gmail.com>
2023-05-15 20:32:59 -0500

    adding all changes

---
+++ src/layout/TopNavbar.vue
@@ -0,0 +1,64 @@
+<template>
+  <nav class="navbar navbar-expand-lg">
+    <li class="nav-item">
+            <a>
+              FECHA
+            </a>
+          </li>
+          <li class="nav-item">
+            <a >
+              USUARIO
+            </a>
+          </li>
+          <li class="nav-item">
+            <a  >
+              EMPRESA
+            </a>
+          </li>
+        <ul class="navbar-nav ml-auto">
+          
+
+          <li class="nav-item">
+            <a href="#" class="nav-link">
+              Log out
+            </a>
+          </li>
+        </ul>
+  </nav>
+</template>
+<script>
+  export default {
+    computed: {
+      routeName () {
+        const {name} = this.$route
+        return this.capitalizeFirstLetter(name)
+      }
+    },
+    data () {
+      return {
+        activeNotifications: false
+      }
+    },
+    methods: {
+      capitalizeFirstLetter (string) {
+        return string.charAt(0).toUpperCase() + string.slice(1)
+      },
+      toggleNotificationDropDown () {
+        this.activeNotifications = !this.activeNotifications
+      },
+      closeDropDown () {
+        this.activeNotifications = false
+      },
+      toggleSidebar () {
+        this.$sidebar.displaySidebar(!this.$sidebar.showSidebar)
+      },
+      hideSidebar () {
+        this.$sidebar.displaySidebar(false)
+      }
+    }
+  }
+
+</script>
+<style>
+
+</style>
---
+++ src/pages/Typography.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>HOLA ESTOY TRABAJANDO</h1>
+</template>
+<script>
+  import Card from 'src/components/Cards/Card.vue'
+
+  export default {
+    components: {
+      Card
+    }
+  }
+
+</script>
+<style>
+
+</style>
