8432ea157f18dd49f838380d8f78f7a40ff45581
eparedes-dev <erick.paredes.nunez@udla.edu.ec>
2023-05-16 21:40:10 -0500

    login de centro de costos con un modal

--- src/layout/DashboardLayout.vue
+++ src/layout/DashboardLayout.vue
@@ -8,7 +8,7 @@
       </sidebar-link>
       <sidebar-link to="/admin/user">
         <i class="nc-icon nc-circle-09"></i>
-        <p>Centro Costos</p>
+        <p @click="OpenLoginModal()">Centro Costos</p>
       </sidebar-link>
       <sidebar-link to="/admin/maps">
         <i class="nc-icon nc-notes"></i>
@@ -40,9 +40,85 @@
 
       <content-footer></content-footer>
     </div>
+    <div class="modal" v-if="CostoLoginModal">
+    <div class="modal-content">
+      <h3>Iniciar sesión</h3>
+      <form @submit.prevent="login">
+        <div class="form-group">
+          <label for="username">Usuario</label>
+          <input type="text" id="username" v-model="username" required>
+        </div>
+        <div class="form-group">
+          <label for="password">Contraseña</label>
+          <input type="password" id="password" v-model="password" required>
+        </div>
+        <button type="submit">Iniciar sesión</button>
+      </form>
+      <button @click="closeLoginModal">Cerrar</button>
+    </div>
   </div>
+  </div>
+  
 </template>
 <style lang="scss">
+.modal {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999;
+}
+
+.modal-content {
+  background-color: #fff;
+  padding: 20px;
+  border-radius: 5px;
+  text-align: center;
+}
+
+.modal-content h3 {
+  margin-top: 0;
+}
+
+.modal-content form {
+  margin-bottom: 20px;
+}
+
+.modal-content .form-group {
+  margin-bottom: 10px;
+}
+
+.modal-content label {
+  display: block;
+  font-weight: bold;
+}
+
+.modal-content input {
+  width: 100%;
+  padding: 8px;
+  border-radius: 5px;
+  border: 1px solid #ccc;
+}
+
+.modal-content button {
+  display: inline-block;
+  padding: 8px 16px;
+  border-radius: 5px;
+  border: none;
+  background-color: #333;
+  color: #fff;
+  cursor: pointer;
+  transition: background-color 0.3s ease;
+}
+
+.modal-content button:hover {
+  background-color: #444;
+}
 
 </style>
 <script>
@@ -51,6 +127,11 @@
   import DashboardContent from './Content.vue'
   import MobileMenu from './MobileMenu.vue'
   export default {
+    data(){
+      return{
+        CostoLoginModal: false
+      }
+    },
     components: {
       TopNavbar,
       ContentFooter,
@@ -62,6 +143,12 @@
         if (this.$sidebar.showSidebar) {
           this.$sidebar.displaySidebar(false)
         }
+      },
+      closeLoginModal(){
+        this.CostoLoginModal = false;
+      },
+      OpenLoginModal(){
+        this.CostoLoginModal = true;
       }
     }
   }
